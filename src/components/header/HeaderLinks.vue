f8b3bc68aa33fceee71e8e0e2ea8f52a11e27245
Alexandr <proxelle@inbox.ru>
2019-09-30 23:13:56 +0300

    add header menu

--- src/components/header/HeaderLinks.vue
+++ src/components/header/HeaderLinks.vue
@@ -1,14 +1,38 @@
 <template lang="pug">
   .header-links
-    input
+    a.header-link(v-for="item in links") {{ item.name }}
 </template>
 
 <script>
 export default {
   name: 'header-links',
-  props: {}
+  props: {},
+  data () {
+    return {
+      links: [
+        { name: 'Главная', link: 'home' },
+        { name: 'Магазин', link: 'home' },
+        { name: 'Ссылки', link: 'home' },
+        { name: 'Публикации', link: 'home' }
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
+  .header-links {
+    display: flex;
+    align-items: center;
+    .header-link {
+      border-left: 1px solid #606034;
+      border-right: 1px solid #606034;
+      color: #606034;
+      margin: 0 -1px 0 0;
+      padding: 0 7px;
+      cursor: pointer;
+      font-weight: bold;
+      line-height: 15px;
+    }
+  }
 </style>

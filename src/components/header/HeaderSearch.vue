d04052fd304ab98f5ad7a0c46b141bcbe1551e97
Александр <kondratiev.as@infotek.ru>
2019-10-01 10:02:37 +0300

    save work

--- src/components/header/HeaderSearch.vue
+++ src/components/header/HeaderSearch.vue
@@ -1,15 +1,33 @@
 <template lang="pug">
   .header-search
-    .header-search-input
+    .header-search-input(:class={'header-search-input-focus': qwe})
       .header-search-icon
-      input
+      input(placeholder="Поиск" @focus="inputFocus" @blur="inputBlur")
       .header-search-clear
 </template>
 
 <script>
 export default {
   name: 'header-search',
-  props: {}
+  props: {},
+  data () {
+    return {
+      focus: true,
+      qwe: true
+    }
+  },
+  methods: {
+    inputFocus () {
+      console.log('inputFocus')
+      this.focus = true
+      console.log(this.focus)
+    },
+    inputBlur () {
+      console.log('inputBlur')
+      this.focus = false
+      console.log(this.focus)
+    }
+  }
 }
 </script>
 
@@ -18,13 +36,24 @@ export default {
   width: 100%;
   display: flex;
   align-items: center;
+  padding: 10px;
   .header-search-input {
     height: 27px;
     border: 1px #bfbfbf solid;
+    box-shadow: inset 0px 1px 2px rgba(0,0,0,0.15);
+    color: white;
+    input {
+      height: 100%;
+      border: none;
+      background-color: #969662;
+      outline: none;
+      &::placeholder {
+        color: white !important;
+      }
+    }
   }
-  input {
-    height: 100%;
+  .header-search-input-focus {
+    border: 1px #CC0000 solid;
   }
 }
-
 </style>

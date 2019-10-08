a2d4313458f17da66e5a9aacb9078cb752598d66
Alexandr <proxelle@inbox.ru>
2019-10-08 23:04:05 +0300

    add component headerSearchItems

--- src/components/header/HeaderSearch.vue
+++ src/components/header/HeaderSearch.vue
@@ -1,50 +1,28 @@
 <template lang="pug">
   .header-search(@blur="closeResultSearch")
     .header-search-input(:class="{'header-search-input-focus': focus}")
-      .header-search-icon(@click="search")
+      .header-search-icon(@click="openResultSearch")
         i.fas.fa-search
-      input(v-modal="input" placeholder="Поиск" @focus="inputFocus" @blur="inputBlur")
-      .header-search-clear(@click="clear")
+      input(v-model="input" placeholder="Поиск" @focus="inputFocus" @blur="inputBlur")
+      .header-search-clear(@click="clearInput")
         i.fas.fa-times-circle
-    .result-search(v-if="showResultSearch")
-          .result-search-title
-            span Найдено совпадений ({{ searchResult.length }})
-            .result-search-nav
-              .result-search-nav-item(v-for="index in navCount()" @click="openPage(index)")
-          .result-search-item
+    header-search-result(v-if="showResultSearch")
 </template>
 
 <script>
+import HeaderSearchResult from './HeaderSearchResult'
+
 export default {
   name: 'header-search',
   props: {},
+  components: {
+    'header-search-result': HeaderSearchResult
+  },
   data () {
     return {
       focus: false,
       input: '',
       showResultSearch: false
-    }
-  },
-  computed: {
-    searchResult () {
-      return [
-        {
-          page: '1',
-          description: '1111111'
-        },
-        {
-          page: '2',
-          description: '2222222'
-        },
-        {
-          page: '3',
-          description: '33333333'
-        },
-        {
-          page: '4',
-          description: '44444444'
-        }
-      ] || []
     }
   },
   methods: {
@@ -54,17 +32,11 @@ export default {
     inputBlur () {
       this.focus = false
     },
-    search () {
-      this.showResultSearch = !this.showResultSearch
+    openResultSearch () {
+      this.showResultSearch = true
     },
-    clear () {
+    clearInput () {
       this.input = ''
-    },
-    navCount () {
-      return Math.ceil(this.searchResult.length / 3)
-    },
-    openPage (index) {
-      console.log(index)
     },
     closeResultSearch () {
       this.showResultSearch = false
@@ -120,48 +92,6 @@ export default {
   }
   .header-search-input-focus {
     border: 1px #CC0000 solid;
-  }
-  .result-search {
-    width: 300px;
-    position: absolute;
-    z-index: 500;
-    top: 30px;
-    left: 0px;
-    background-color: white;
-    -webkit-box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.3);
-    box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.3);
-    .result-search-title {
-      background-color: #F2F2F2;
-      border-top: 1px solid #cdcdcd;
-      text-transform: uppercase;
-      height: 27px;
-      span {
-        display: inline-block;
-        color: #4E6170;
-        margin-left: 10px;
-        margin-top: 5px;
-      }
-      .result-search-nav {
-        display: inline-block;
-        float: right;
-        height: 20px;
-        margin-right: 6px;
-        .result-search-nav-item {
-          height: 6px;
-          width: 6px;
-          border-radius: 5px;
-          border: 2px solid #4E6170;
-          background-color: #4E6170;
-          display: inline-block;
-          margin-left: 2px;
-          margin-top: 8px;
-          cursor: pointer;
-        }
-        .result-search-nav-item-active {
-          background-color: transparent;
-        }
-      }
-    }
   }
 }
 </style>
